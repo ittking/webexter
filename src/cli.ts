d7f75ba024128c9f3cdbcae6b0d3e975cb344f4f
mnwm <mnwm@noreply.gitcode.com>
2026-05-09 14:38:44 +0800

    feat: 更新版本号至 1.0.1，并从 package.json 中获取版本信息

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
+/*
+ * @Author: mnwm mnwm@noreply.gitcode.com
+ * @Date: 2026-05-09 14:37:39
+ * @LastEditors: mnwm mnwm@noreply.gitcode.com
+ * @LastEditTime: 2026-05-09 14:37:53
+ * @FilePath: /webexter/src/cli.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 import { Command } from 'commander'
 import { create } from './commands/create.js'
+import pkg from '../package.json'
 
 const program = new Command()
 
 program
   .name('webexter')
   .description('A scaffolding CLI for quickly building browser extensions')
-  .version('1.0.0')
+  .version(pkg.version)
 
 program
   .command('create')
